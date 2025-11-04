95f2a9882f3a0ce3d9a5617266500af034c33a97
Leonid Gordenin <db0zmail@gmail.com>
2025-11-04 15:55:25 -0300

    WheelConfig system

--- scripts/CarWheel.cs
+++ scripts/CarWheel.cs
@@ -1,25 +1,16 @@
 using Godot;
 using System;
+using racingGame;
 
 public partial class CarWheel : ShapeCast3D
 {
-	[ExportCategory("Suspension")]
-	[Export] public int SpringStrength = 100;
-	[Export] public float SpringDamping = 2.0f;
-	[Export] public float SpringRest = 0.5f;
-	[Export] public float OverExtend = 0.2f;
+	[Export] public WheelConfig Config;
 	
-	[ExportCategory("Wheel Parameters")]
-	[Export] public float WheelRadius = 0.5f;
-	[Export] public bool IsDriveWheel = false;
-	[Export] public bool IsSteerWheel = false;
-
-	[ExportCategory("Curves")]
-	[Export] public Curve GripCurve;
+	[ExportCategory("Setup")]
+	[Export] public bool IsFrontWheel;
+	[Export] public bool IsRearWheel;
 
 	public Node3D WheelModel;
-
-	public bool IsSlipping = false;
 	
 	public override void _Ready()
 	{
